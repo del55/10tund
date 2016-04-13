9ef0f939d3ad0007aaf24c4a2e9d751e999bb171
Delvis Ramot <dramot@greeny.cs.tlu.ee>
2016-04-13 10:19:21 +0300

    javascript külge

---
+++ feed.js
@@ -0,0 +1,30 @@
+$(function() {
+
+  //laetud
+
+  var content = $("#content");
+
+  getTweets();
+
+
+});
+
+function getTweets(){
+	//ajax
+
+	$.ajax({
+		url: "getfeed.php",
+		success: function(data){
+
+			//stringi teen massiiviks
+			var array = JSON.parse(data).statuses;
+
+			console.log(array);
+
+		},
+		error: function(error){
+			console.log(error);
+		}
+	});
+
+}
