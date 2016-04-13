4e531c83529c9380a13acf25e2b110514cff9085
Delvis Ramot <dramot@greeny.cs.tlu.ee>
2016-04-13 09:20:18 +0300

    kirjutab txt faili

--- getfeed.php
+++ getfeed.php
@@ -1,6 +1,7 @@
 <?php
 require_once("TwitterAPIExchange.php");
 require_once("config.php");
+$file_name = "cache.txt";
 
 $url = "https://api.twitter.com/1.1/search/tweets.json";
 $getField = "?q=%23Paris&result_type=recent";
@@ -10,5 +11,15 @@ $twitter = new TwitterAPIExchange($config);
 $dataFromAPI = $twitter->setGetfield($getField)
 ->buildOauth($url, $requestMethod)
 ->performRequest();
-echo $dataFromAPI;
+
+
+//var_dump(json_decode($dataFromAPI)->statuses);
+$object = new StdClass();
+//millal tegime päringu
+$object -> date = date("c");
+
+//saadud tweedid
+$object->statuses = json_decode($dataFromAPI)->statuses;
+file_put_contents($file_name, json_encode($object));
+echo json_encode($object);
  ?>
