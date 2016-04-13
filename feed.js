905c18a95a9dd0313ea6cd18bd466b5a2c0091da
Delvis Ramot <dramot@greeny.cs.tlu.ee>
2016-04-13 10:38:04 +0300

    vorming hea, css puudu

--- feed.js
+++ feed.js
@@ -2,14 +2,14 @@ $(function() {
 
   //laetud
 
-  var content = $("#content");
-
   getTweets();
 
 
 });
 
+
 function getTweets(){
+
 	//ajax
 
 	$.ajax({
@@ -20,11 +20,32 @@ function getTweets(){
 			var array = JSON.parse(data).statuses;
 
 			console.log(array);
+			printTweets(array);
 
 		},
 		error: function(error){
 			console.log(error);
 		}
 	});
+
+}
+
+function printTweets(newTweets){
+
+	var html = '';
+
+	$(newTweets).each(function(i, tweet){
+
+		html += '<div class="item">'+
+
+			'<div class="profile-image" style="background-image:url('+tweet.user.profile_image_url+')"></div>'+
+			'<p>'+tweet.user.name+'</p>'+
+			'<p>'+tweet.text+'</p>'+
+
+		'</div>';
+
+	});
+
+	$("#content").append( $(html) );
 
 }
